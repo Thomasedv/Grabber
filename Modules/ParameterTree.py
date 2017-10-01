e94e86653a95b133f19ce4da5e8733ce47f755cc
Thomasedv <Thomasedv@users.noreply.github.com>
2017-10-01 18:28:24 +0200

    Renamed and documented larger parts of formerly paramTree.
    Now called ParameterTree. Got some type indications for a lot of it.

--- Modules/ParameterTree.py
+++ Modules/ParameterTree.py
@@ -1,5 +1,5 @@
 import sys
-
+from typing import Union, Optional
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem
 
@@ -8,10 +8,10 @@ class ParameterTree(QTreeWidget):
     def __init__(self, dicts: dict):
         super().__init__()
         """
-        data table:
-        All data is in row 0. 
+        Data table:
+        All data is in column 0. 
         --
-        0  -
+        0  - Visual name. 
         32 - main data entry, name of parents, full data for children
         33 - 0 for parent, 1 for children
         34 - Name of item that needs to be checked
@@ -32,30 +32,34 @@ class ParameterTree(QTreeWidget):
             if settings['options'] is not None:
                 for number, choice in enumerate(settings['options']):
                     if settings['Active option'] == number:
-                        sub = self.makeOption(choice, parent, True, 1, subindex=number)
-                        sub.setFlags(sub.flags() ^ Qt.ItemIsUserCheckable)
+                        option = self.makeOption(choice, parent, True, 1, subindex=number)
+                        option.setFlags(option.flags() ^ Qt.ItemIsUserCheckable)
                     else:
-                        sub = self.makeOption(choice, parent, False, 1, subindex=number)
-            self.makeExclusive(parent)
+                        option = self.makeOption(choice, parent, False, 1, subindex=number)
+            self.make_exclusive(parent)
 
-        self.hockDependecy()
-        self.itemChanged.connect(self.makeExclusive)
-        self.itemChanged.connect(self.checkDependecny)
-        self.startSize()
+        self.hock_dependency()
 
-    def hockDependecy(self):
-        toplevelnames = []
-        for i in self.topLevelItems():
+        self.itemChanged.connect(self.make_exclusive)
+        self.itemChanged.connect(self.check_dependency)
+
+        self.start_size()
+
+    def hock_dependency(self):
+        top_level_names = []
+        for item in self.topLevelItems():
             # Create a list if top level items, their dependent and the QModelIndex of said item.
-            toplevelnames.append([i.data(0, 32), i.data(0, 34), self.indexFromItem(i)])
+            top_level_names.append([item.data(0, 32), item.data(0, 34), self.indexFromItem(item)])
+
         # Locate matches, and store in dict
-        indices = {t[0]: i for i, t in enumerate(toplevelnames)}
-        for index, (first, second, third) in enumerate(toplevelnames):
+
+        indices = {t[0]: i for i, t in enumerate(top_level_names)}
+        for index, (first, second, third) in enumerate(top_level_names):
             if second in indices:
                 # Locate matches and assign dependent reference.
                 dependent = self.itemFromIndex(third)
                 # Locate host (that dependent needs to have checked)
-                host = self.itemFromIndex(toplevelnames[indices[second]][2])
+                host = self.itemFromIndex(top_level_names[indices[second]][2])
 
                 # Check if there is a reference to other dependents.
                 # If True, make add a new dependency, if not, make a list with a dependency.
@@ -63,18 +67,18 @@ class ParameterTree(QTreeWidget):
                     host.setData(0, 37, host.data(0, 37) + [dependent])
                 else:
                     host.setData(0, 37, [dependent])
+
         # Ensure dependents are disabled on start!
         for item in self.topLevelItems():
-            self.checkDependecny(item)
+            self.check_dependency(item)
 
-    def checkDependecny(self, item: QTreeWidgetItem):
+    def check_dependency(self, item: QTreeWidgetItem):
         """
-        Looks for mentioned dependents, and enables/disables those depending on chekcstate.
+        Looks for mentioned dependents, and enables/disables those depending on checkstate.
 
         :param item: changed item from QTreeWidget (paramTree)
         :type item: QTreeWidget
         """
-
         if item.data(0, 33) == 0 and item.data(0, 37):
             for i in item.data(0, 37):
                 if item.checkState(0) == Qt.Unchecked:
@@ -88,38 +92,45 @@ class ParameterTree(QTreeWidget):
                     self.blockSignals(False)
 
     def topLevelItems(self):
+        """Iterates through toplevelitems and returns them."""
         for i in range(self.topLevelItemCount()):
             yield self.topLevelItem(i)
 
     @staticmethod
-    def makeOption(name, parent, checkstate, level=0, tooltip=None, dependency=None, subindex=None):
-        # implemter data 33 for parent/child.
-        #
-        # print(name)
-        mainItem = QTreeWidgetItem(parent, [name])
+    def makeOption(name: str,
+                   parent: Union[QTreeWidget, QTreeWidgetItem],
+                   checkstate: bool,
+                   level: int = 0,
+                   tooltip: Optional[str] = None,
+                   dependency: Optional[list] = None,
+                   subindex: Optional[int] = None)\
+            -> QTreeWidgetItem:
+        """
+        Makes a QWidgetItem and returns it.
+        """
+        widget_item = QTreeWidgetItem(parent, [name])
         if tooltip:
-            mainItem.setToolTip(0, tooltip)
+            widget_item.setToolTip(0, tooltip)
         if checkstate:
-            mainItem.setCheckState(0, Qt.Checked)
+            widget_item.setCheckState(0, Qt.Checked)
         else:
-            mainItem.setCheckState(0, Qt.Unchecked)
+            widget_item.setCheckState(0, Qt.Unchecked)
 
-        mainItem.setData(0, 32, name)
-        mainItem.setData(0, 33, level)
+        widget_item.setData(0, 32, name)
+        widget_item.setData(0, 33, level)
 
         if level == 1:
-            mainItem.setData(0, 35, subindex)
+            widget_item.setData(0, 35, subindex)
         elif level == 0:
             if dependency:
-                mainItem.setData(0, 34, dependency)
-        # mainItem.setChildIndicatorPolicy(QTreeWidgetItem.DontShowIndicator)
-        # mainItem.setExpanded(True)
+                widget_item.setData(0, 34, dependency)
 
-        return mainItem
+        return widget_item
 
-    def startSize(self):
-        size = sum([1 for i in range(self.topLevelItemCount()) for x in range(self.topLevelItem(i).childCount())])
-        # print(size)
+    def start_size(self):
+        size = sum([1 for i in range(self.topLevelItemCount()) for _ in range(self.topLevelItem(i).childCount())])
+        # Unhandled lengths when the program exceeds the window size. Might implement a max factor, and allow scrolling.
+        # Future cases might implement two ParameterTrees side by side, for better use of space and usability.
         self.setFixedHeight(20 * self.topLevelItemCount() + 15 * size)
 
     def exclusive(self, item: QTreeWidgetItem):
@@ -129,21 +140,18 @@ class ParameterTree(QTreeWidget):
             item.setExpanded(False)
 
     def resizer(self, item: QTreeWidgetItem):
-        try:
-            # print('Child count', item.childCount())
-            if item.checkState(0):
-                self.setFixedHeight(self.height() + 15 * item.childCount())
-                # print('Expanding')
-            else:
-                self.setFixedHeight(self.height() - 15 * item.childCount())
-                # print('Collapsing')
-        except Exception as e:
-            print(e)
+        # print('Child count', item.childCount())
+        if item.checkState(0):
+            self.setFixedHeight(self.height() + 15 * item.childCount())
+            # print('Expanding')
+        else:
+            self.setFixedHeight(self.height() - 15 * item.childCount())
+            # print('Collapsing')
 
-    def makeExclusive(self, item: QTreeWidgetItem):
-        # print(type(item), type(test))
-        # print(type(item.checkState()))
-        # if item.checkState() ==
+    def make_exclusive(self, item: QTreeWidgetItem):
+        """
+        Handles changes to self. Ensure options are exclusive, and resizes self when needed.
+        """
         if item.data(0, 33) == 0:
             self.exclusive(item)
             self.resizer(item)
