a2e2fc77e5d416ff0676acc97d3cc9baa156417a
Thomasedv <Thomasedv@users.noreply.github.com>
2017-10-01 18:39:33 +0200

    Better main file. Easier to understand.

--- Main.py
+++ Main.py
@@ -7,22 +7,31 @@ from PyQt5.QtWidgets import QApplication
 def main():
     EXIT_CODE = GUI.EXIT_CODE_REBOOT
 
-    while EXIT_CODE == -123456789:
+    while True:
         try:
             app = QApplication(sys.argv)
             qProcess = GUI()
 
             EXIT_CODE = app.exec_()
-            app= None
+            app = None
+
+
+            if EXIT_CODE == -123456789:
+                continue
+            else:
+                break
 
         except (SettingsError,json.decoder.JSONDecodeError) as e :
-            A = QMessageBox.warning(None, 'Corrupt settings', ''.join([str(e),'\nRestore default settings?']), buttons=QMessageBox.Yes | QMessageBox.No)
-            if A == QMessageBox.Yes:
+            warning = QMessageBox.warning(None,
+                                          'Corrupt settings',
+                                          ''.join([str(e),'\nRestore default settings?']),
+                                          buttons=QMessageBox.Yes | QMessageBox.No)
+            if warning == QMessageBox.Yes:
                 GUI.write_default_settings(True)
-                EXIT_CODE = -123456789
+                app = None
+                continue
             else:
-                EXIT_CODE = 1
-            app = None
+                break
 
 if __name__ == '__main__':
     main()
